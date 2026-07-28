f76283575b938b33481486663f8a93ae1f9ccabd
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-07-28 22:50:38 +0300

    ДЗ 23.2, созданы модели Product и Category, описаны базовые настройки

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.db.models import SET_NULL
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Наименование',
+                            help_text='Введите наименование категории')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание',
+                                   help_text='Введите описание категории')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Наименование',
+                            help_text='Введите наименование продукта')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание',
+                                   help_text='Введите описание продукта')
+    image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='Изображение',
+                              help_text='Загрузите изображение продукта')
+    category = models.ForeignKey(Category, on_delete=SET_NULL, blank=True, null=True, related_name='products',
+                                 help_text='Выберите категорию продукта')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку',
+                                help_text='Введите цену')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name', 'category']
--- config/settings.py
+++ config/settings.py
@@ -91,4 +91,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
