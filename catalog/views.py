ae3cea070ebb4ae6fce56b2fecadf06be2bfb3f7
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-08-06 13:36:34 +0300

    ДЗ 24.1 Создан контроллер и шаблон для страницы с подробной информацией о товаре

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
@@ -16,3 +18,9 @@ def contacts(request):
         print(message)
         return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
     return render(request, 'contacts.html')
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
