676f3decd23291b71b583a80ef9856c419e4e3a9
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-08-06 21:14:34 +0300

    ДЗ 24.2 В шаблон главной страницы добавлен код для отображения списка товаров. Обновлен шаблон страницы с подробной информацией о товаре.

--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,9 @@ from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context)
 
 
 def contacts(request):
