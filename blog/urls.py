85cb4823c6530ef4ea2ece187da2e3d10dd47c5b
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-08-19 11:02:26 +0300

    ДЗ 25 Исправлена ошибка url адреса

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('', ArticleListView.as_view(), name='list'),
     path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('articles/create', ArticleCreateView.as_view(), name='article_create'),
+    path('articles/create/', ArticleCreateView.as_view(), name='article_create'),
     path('articles/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
     path('articles/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
 ]
