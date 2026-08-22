22950515f39ad261083b3e8481bdf0dd8da0142b
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-08-23 02:30:48 +0300

    ДЗ 26

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductDetailView, ProductListVieW, ContactsTemplateView
+from catalog.views import ProductDetailView, ProductListView, ContactsTemplateView, ProductCreateView, \
+    ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListVieW.as_view(), name='list'),
+    path('', ProductListView.as_view(), name='list'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from catalog.models import Product
+from catalog.forms import ProductForm
+from catalog.models import Product, Category
 
 
-class ProductListVieW(ListView):
+class ProductListView(ListView):
     model = Product
     template_name = 'catalog/home.html'
     context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:list')
+
+    def form_valid(self, form):
+        new_category_name = form.cleaned_data.get('new_category_name')
+        new_category_description = form.cleaned_data.get('new_category_description')
+
+        # Если пользователь решил создать новую категорию
+        if new_category_name:
+            # Создаем и сохраняем новую категорию в БД
+            new_category = Category.objects.create(
+                name=new_category_name,
+                description=new_category_description
+            )
+            # Привязываем свежесозданную категорию к объекту продукта
+            form.instance.category = new_category
+
+        return super().form_valid(form)
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:list')
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:list')
 
 
 class ContactsTemplateView(TemplateView):
@@ -26,7 +69,3 @@ class ContactsTemplateView(TemplateView):
         context['success'] = True
         context['user_name'] = name
         return render(request, self.template_name, context)
-
-
-class ProductDetailView(DetailView):
-    model = Product
