5e234eced75f9b25938233b8f6b2b8b7db24ed83
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-09-04 23:23:48 +0300

    ДЗ 27.2 Реализована аутентификация

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -115,3 +115,6 @@ BLACKLIST = ['казино', 'криптовалюта', 'крипта', 'бир
 PHONENUMBER_DEFAULT_REGION = "RU"
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from config import settings
+from users.forms import UserRegisterForm
+
+
+class RegisterView(CreateView):
+    template_name = 'users/register.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = 'Добро пожаловать на сайт "Мой сад"!'
+        message = 'Поздравляем с успешной регистрацией!'
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipient_list = [user_email, ]
+        send_mail(subject, message, from_email, recipient_list)
