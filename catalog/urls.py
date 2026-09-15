aa7a75f1ca538b9b5ed259b5653112d3728e711b
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-09-16 00:03:59 +0300

    ДЗ 29.2 Настроено кэширования для страницы одного продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductDetailView, ProductListView, ContactsTemplateView, ProductCreateView, \
     ProductUpdateView, ProductDeleteView, ProductUnpublishView
@@ -8,7 +10,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='list'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
